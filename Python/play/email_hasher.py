2b348cdd592bdab023eb785b09f74b9ea3482056
Tapajit Dey <tapjdey@gmail.com>
2021-01-21 22:36:20 +0000

    Update email_hasher.py

--- Python/play/email_hasher.py
+++ Python/play/email_hasher.py
@@ -1,20 +1,32 @@
-import hashlib, gzip
+# Run as python3 <email_hasher.py <input file> <output file> <field to convert to hash in the csv - optional>
+# FS etc. changed in code
 
-'''Convert Author ID to SHA '''
+import hashlib, gzip, sys
 
 def hasher(string):
     return str(hashlib.sha1(string.encode()).hexdigest())
 
 def main():
-    newfile = gzip.open('ghtNE2aQ.forMLF.hashed.gz', 'wt', encoding = 'latin-1')
-    with gzip.open('ghtNE2aQ.forMLF2.gz','rt', encoding = 'latin-1') as f:
+    of = sys.argv[2]
+    inf = sys.argv[1]
+    try:
+        fld = int(sys.argv[3])
+    except:
+        fld = 0.1
+    newfile = gzip.open(of, 'wt')
+    with gzip.open(inf,'rt', encoding = 'latin-1') as f:
         for line in f:
-            line = (line.strip()).replace('#','@')
+            line = (line.strip())
             parts = line.split(';')
-            hashed = ';'.join([hasher(x) if i == 4 or i == 5 else x for i,x in enumerate(parts)])
+            if fld == 0.1:
+            	hashed = ';'.join([hasher(x) for x in parts])
+            else:
+                for i in range(len(parts)):
+                   if i == fld: parts[i] = hasher(parts[i])
+                hashed = ';'.join(parts)
             newfile.write(hashed+'\n')
-            
+
     newfile.close()
-            
+
 if __name__ == '__main__':
     main()
